d6dd87b1080fc03f796efbeaa2174414e52eeecc
Conradgabe <gisuekebho5880@gmail.com>
2023-09-23 19:30:56 +0100

    modified main.py

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ import gi
 os.environ["LC_ALL"] = "C"
 
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk#
+from gi.repository import Gtk, GdkPixbuf, Gio
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,7 +17,12 @@ class WeatherTrayApp:
     def __init__(self):
         self.icon = Gtk.StatusIcon()
         self.icon.set_from_file("icon.png")
-        self.icon.connect("activate", self.on_activate)
+        self.create_icon()
+
+    def create_icon(self):
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file("./icon.png")
+        image = Gtk.Image.new_from_pixbuf(pixbuf)
+        self.icon.set_from_pixbuf(pixbuf)
 
     def on_activate(self, icon):
         response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=London&appid=" + API_SECRET_KEY)
